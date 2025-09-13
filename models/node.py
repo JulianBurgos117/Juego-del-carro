f40e96b582364dc7190e55bd2fd85f78d83cee4b
Juanfe1609 <162798762+Juanfe1609@users.noreply.github.com>
2025-09-13 14:09:49 -0500

    Update node.py
    aqui actualice la clase nodo para que trabaje con coordenadas, y un tipo para los obstaculos

--- models/node.py
+++ models/node.py
@@ -1,10 +1,13 @@
  #building class of nodes
 class Node:
-    
-    def __init__(self, value, parent=None):
+    def __init__(self, value, tipe):
+        """
+        value: tuple (x, y) -> obstacle coordinates
+        tipo: string -> obstacle tipe (stone, hole, etc.)
+        """
         self.value = value
+        self.tipo = tipe
+        self.height = 1
         self.left = None
         self.right = None
-        self.parent = parent
-        self.height = 1
         
